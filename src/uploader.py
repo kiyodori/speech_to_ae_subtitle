6bd19b6fa2142a5175eaaf107477be659ac16dfe
kiyodori <kiyodori@yahoo.co.jp>
2021-08-29 19:54:39 +0900

    add fire to uploader

--- src/uploader.py
+++ src/uploader.py
@@ -5,3 +5,6 @@ def upload(file, bucket):
   s3 = boto3.resource('s3')
   data = open(f'audio/{file}', 'rb')
   s3.Bucket(bucket).put_object(Key=file, Body=data)
+
+if __name__ == '__main__':
+  fire.Fire(upload)
