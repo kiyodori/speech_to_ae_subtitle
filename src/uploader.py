3aca6e3ebfd66405fe6a8773d821bff8f075b0c9
kiyodori <kiyodori@yahoo.co.jp>
2021-09-08 16:13:52 +0900

    fix format by black

--- src/uploader.py
+++ src/uploader.py
@@ -2,12 +2,14 @@ import boto3
 import fire
 import config
 
-def upload(file):
-  s3 = boto3.resource('s3',
-    aws_access_key_id=config.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY)
-  data = open(f'audio/{file}', 'rb')
-  s3.Bucket(config.S3_INPUT_BUCKET_NAME).put_object(Key=file, Body=data)
 
-if __name__ == '__main__':
-  fire.Fire(upload)
+def upload(file):
+    s3 = boto3.resource(
+        "s3", aws_access_key_id=config.AWS_ACCESS_KEY_ID, aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
+    )
+    data = open(f"audio/{file}", "rb")
+    s3.Bucket(config.S3_INPUT_BUCKET_NAME).put_object(Key=file, Body=data)
+
+
+if __name__ == "__main__":
+    fire.Fire(upload)
